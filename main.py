ee21003512f8d88d032ac257de522022540e952b
somebody241 <andreev.venko07@gmail.com>
2025-05-08 13:02:51 +0300

    switch_api

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import pyttsx3
  
 r = sr.Recognizer() 
 
+
 def SpeakText(command):
     engine = pyttsx3.init()
-    engine.say(command) 
+    engine.say(command)
     engine.runAndWait()
 
 
-while(1):    
+while 1 == 1:
     try:
         with sr.Microphone() as source2:
             r.adjust_for_ambient_noise(source2, duration=0.2)
             audio2 = r.listen(source2)
-            MyText = r.recognize_google(audio2)
+            MyText = r.recognize_ibm(audio2, key="Any", language="en-US")
             MyText = MyText.lower()
-            print("Did you say ",MyText)
+            print("Did you say ", MyText)
             SpeakText(MyText)
             
     except sr.RequestError as e:
