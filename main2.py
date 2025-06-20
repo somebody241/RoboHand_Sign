956aa9a548c063d8fd5e29d10ad621f748db983b
ValentinBerberov <valentin.berberov007@gmail.com>
2025-06-20 15:40:44 +0300

    Fix?

--- main2.py
+++ main2.py
@@ -7,21 +7,28 @@ import code
 
 alphabet = ['а', 'б', 'в', 'г', 'е', 'и', 'л', 'м', 'н', 'о', 'п', 'р', 'т', 'у', 'х', 'ц', 'ч', 'ш', 'ю']
 
+left_hand_letters = ['л', 'м', 'п', 'т']
+
+rh = code.right_hand
+lh = code.left_hand
 
 model = whisper.load_model("base")
 sample_rate = 16000
-dur = 5
+dur = 3
 running = True
 
 print("SPACE to start/stop")
 print("ESC to exit")
 
-audio = sd.rec(int(dur * sample_rate), samplerate=sample_rate, channels=1, dtype=np.float32, frames=48000)
+audio = sd.rec(int(dur * sample_rate), samplerate=sample_rate, channels=1, dtype=np.float32)
 sd.wait()
-result = model.transcribe(audio.flatten(), language="bg")
+result = model.transcribe(audio.flatten(), language="bg")['text']
 arr = list(result)
 print("result: " + str(arr))
 if set(arr).issubset(alphabet):
     for i in arr:
-        map.table(i)
+        if i in left_hand_letters:
+            map.table[i](lh)
+        else:
+            map.table[i](rh)
 code.board.close()
