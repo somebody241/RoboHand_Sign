4c2224ecaffa933993913668c5c225c6b8fcc860
somebody241 <andreev.venko07@gmail.com>
2025-06-20 15:08:03 +0300

    push

--- code.py
+++ code.py
@@ -2,7 +2,8 @@ from pyfirmata import Arduino
 
 from sign_letters import *
 
-board = Arduino('/dev/cu.usbmodem1101')
+# board = Arduino('/dev/cu.usbmodem1101')
+# board = Arduino('COM7')
 
 right_pinky = board.get_pin("d:2:s")
 right_ring = board.get_pin("d:4:s")
